72a4604e60f3ead9e5bbbceacb0f277743b3c01c
NyanShen <3354509516@qq.com>
2019-07-17 17:36:30 +0800

    add wx conifg

---
+++ routes/pay/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    wx: {
+        appId: 'wx01f3318e69a9b809',
+        appSecret: '0c1150f462a3a86344707ee57b6fb608'
+    }
+}
--- routes/pay/wx.js
+++ routes/pay/wx.js
@@ -1,13 +1,28 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const request = require('request')
+const memoryCache = require('memory-cache')
+const config = require('./config')
+const router = express.Router()
 
-router.get('/test', function(req, res){
+router.get('/test', function (req, res) {
     res.json({
-        code: 200,
+        code: 0,
         data: {
             test: "Nyan Test"
         },
         message: ''
     })
+})
+router.get('/redirect', function (req, res) {
+    const redirectUrl = req.query.url;
+    memoryCache.put('redirectUrl', redirectUrl)
+    const scope = req.query.scope;
+    const callbackUrl = 'http://cm.sym.com/api/wechat/getOpenId';
+    const authrizeUrl = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${config.wx.appId}&redirect_uri=${callbackUrl}&response_type=code&scope=${scope}&state=STATE#wechat_redirect`;
+    res.redirect(authrizeUrl)
+})
+
+router.get('/getOpenId', function(req, res) {
+    
 })
 module.exports = router
