21a45d87babce0d61904f21f531fb481f58142b6
NyanShen <3354509516@qq.com>
2019-07-18 17:35:49 +0800

    update openId interface

--- routes/pay/wx.js
+++ routes/pay/wx.js
@@ -4,25 +4,38 @@ const memoryCache = require('memory-cache')
 const config = require('./config')
 const router = express.Router()
 
-router.get('/test', function (req, res) {
-    res.json({
-        code: 0,
-        data: {
-            test: "Nyan Test"
-        },
-        message: ''
-    })
-})
 router.get('/redirect', function (req, res) {
-    const redirectUrl = req.query.url;
-    memoryCache.put('redirectUrl', redirectUrl)
     const scope = req.query.scope;
-    const callbackUrl = 'http://cm.sym.com/api/wechat/getOpenId';
-    const authrizeUrl = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${config.wx.appId}&redirect_uri=${callbackUrl}&response_type=code&scope=${scope}&state=STATE#wechat_redirect`;
-    res.redirect(authrizeUrl)
+    const redirect_url = req.query.url;
+    memoryCache.put('redirect_url', redirect_url)
+    const callback_url = 'http://cm.sym.com/api/wechat/getOpenId';
+    const authrize_url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${config.wx.appId}&redirect_uri=${callback_url}&response_type=code&scope=${scope}&state=STATE#wechat_redirect`;
+    res.redirect(authrize_url)
 })
 
-router.get('/getOpenId', function(req, res) {
-    
+router.get('/getOpenId', function (req, res) {
+    //http://cm.sym.com/api/wechat/getOpenId?code=CODE&state=STATE
+    const code = req.query.code;
+    console.log('code:',code)
+    const token_url = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${config.wx.appId}&secret=${config.wx.appSecret}&code=${code}&grant_type=authorization_code`;
+    if (!code) {
+        res.json({
+            code: 1001,
+            data: '',
+            message: '当前未获取到授权码'
+        })
+    } else {
+        request.get(token_url, function (err, response, body) {
+            if (!err && response.statusCode == 200) {
+                const data = JSON.parse(body)
+                console.log('body:', body)
+                const expire_time = 1000 * 60 * 1
+                res.cookie('openId', data.openId, { maxAge: expire_time })
+                res.redirect(memoryCache.get('redirect_url'))
+            } else {
+
+            }
+        })
+    }
 })
 module.exports = router
